2a4a8f1334a0c2e9752436412e4a1aa192672996
abdulkadir <abdcelikkanat@gmail.com>
2020-06-03 22:39:24 +0200

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
-edgelistbase="../nodesig_edgelists"
-outputbase="../nodesig_embeddings"
+edgelistbase="../node2sig_edgelists"
+outputbase="../node2sig_embeddings"
 walklen=5
 
 datasets=(blogcatalog dblp Homo_sapiens wiki)
@@ -9,15 +9,19 @@ datasets=(blogcatalog dblp Homo_sapiens wiki)
 walk_lens=(1 2 3 4 5)
 
 
-for dataset in ${datasets[@]}
+for L in ${walk_lens[@]}
 do
+        for dataset in ${datasets[@]}
 	
-	for L in ${walk_lens[@]}
 	do
 		edge_file_path=${edgelistbase}/${dataset}_newborn.edgelist
 		emb_file_path=${outputbase}/${dataset}_newborn_L=${L}.edgelist
 		
-		./nodesig --edgefile ${edge_file_path} --embfile ${emb_file_path} --walklen ${walklen}
+                echo "---------------"
+		echo $dataset, $L
+                echo "---------------"
+
+		./nodesig --edgefile ${edge_file_path} --embfile ${emb_file_path} --walklen ${L}
 		
 	done
 done
